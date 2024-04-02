c008d7589d26acf0a80802784f5b8902e3c429b0
RAYNF <rayhannoor33@gmail.com>
2024-04-02 22:46:48 +0700

    updateProjectSplashScreen
    update project SplashScreen Pantau Semar

---
+++ pantau_semar/lib/ui/splashscreen_page.dart
@@ -0,0 +1,39 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatefulWidget {
+  static const routeName = '/splash-screen';
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Color(0xffF4F0F0),
+      body: SafeArea(
+          child: Center(
+        child: Column(
+          children: [
+            SizedBox(height: screenSize.height/5.5),
+            Image.asset('assets/logo_splashscreen.png'),
+            Text(
+              "PantauSemar",
+              style: GoogleFonts.poppins(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xffD93A3A)),
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
