242bb925b7a2d75d75ef9a94e4eb1cce7fe196d5
RAYNF <rayhannoor33@gmail.com>
2024-05-23 16:03:03 +0700

    update

--- pantau_semar/lib/widget/vidioplayer_widget.dart
+++ pantau_semar/lib/widget/vidioplayer_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pantau_semar/utils/Theme.dart';
 import 'package:video_player/video_player.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class VideoPlayerScreen extends StatefulWidget {
   const VideoPlayerScreen({super.key});
@@ -10,28 +11,36 @@ class VideoPlayerScreen extends StatefulWidget {
 }
 
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
-  late VideoPlayerController _controller;
-  late Future<void> _initializeVideoPlayerFuture;
+  // late VideoPlayerController _controller;
+  // late Future<void> _initializeVideoPlayerFuture;
+  final videoURL = "https://www.youtube.com/watch?v=YMx8Bbev6T4";
+  late YoutubePlayerController _controller;
+
   // double speed = 1;
 
   @override
   void initState() {
     super.initState();
+    final videoID = YoutubePlayer.convertUrlToId(videoURL);
+    _controller = YoutubePlayerController(
+      initialVideoId: videoID!,
+      flags: const YoutubePlayerFlags(autoPlay: false),
+    );
 
     // Create and store the VideoPlayerController. The VideoPlayerController
     // offers several different constructors to play videos from assets, files,
     // or the internet.
-    _controller = VideoPlayerController.networkUrl(
-      Uri.parse(
-        'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
-      ),
-    );
+    // _controller = VideoPlayerController.networkUrl(
+    //   Uri.parse(
+    //     'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    //   ),
+    // );
 
-    // Initialize the controller and store the Future for later use.
-    _initializeVideoPlayerFuture = _controller.initialize();
+    // // Initialize the controller and store the Future for later use.
+    // _initializeVideoPlayerFuture = _controller.initialize();
 
-    // Use the controller to loop the video.
-    _controller.setLooping(true);
+    // // Use the controller to loop the video.
+    // _controller.setLooping(true);
     // _controller.setPlaybackSpeed(speed);
   }
 
@@ -47,74 +56,19 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Stack(
-          //bagian mp4
-
-          children: [
-            Container(
-                decoration: BoxDecoration(color: danger),
-                height: 200,
-                width: double.infinity,
-                child: InkWell(
-                  onTap: () {},
-                  child: FutureBuilder(
-                    future: _initializeVideoPlayerFuture,
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.done) {
-                        // If the VideoPlayerController has finished initialization, use
-                        // the data it provides to limit the aspect ratio of the video.
-                        return AspectRatio(
-                          aspectRatio: _controller.value.aspectRatio,
-                          // Use the VideoPlayer widget to display the video.
-                          child: VideoPlayer(_controller),
-                        );
-                      } else {
-                        // If the VideoPlayerController is still initializing, show a
-                        // loading spinner.
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                    },
-                  ),
-                )),
-            Positioned.fill(
-              child: Align(
-                alignment: Alignment.center,
-                child: IconButton(
-                  onPressed: () {
-                    // Wrap the play or pause in a call to `setState`. This ensures the
-                    // correct icon is shown.
-                    setState(() {
-                      // If the video is playing, pause it.
-                      if (_controller.value.isPlaying) {
-                        _controller.pause();
-                      } else {
-                        // If the video is paused, play it.
-                        _controller.play();
-                      }
-                    });
-                  },
-                  // Display the correct icon depending on the state of the player.
-                  icon: Icon(
-                    _controller.value.isPlaying
-                        ? Icons.pause
-                        : Icons.play_arrow,
-                    color: danger,
-                    size: 40,
-                  ),
-                ),
-              ),
+        YoutubePlayer(
+          controller: _controller,
+          showVideoProgressIndicator: true,
+          onReady: () => debugPrint("READY"),
+          bottomActions: [
+            CurrentPosition(),
+            ProgressBar(
+              isExpanded: true,
+              colors:
+                  ProgressBarColors(playedColor: danger, handleColor: primary),
             )
-            //memepercepat
-            // IconButton(onPressed: (){
-            //   setState(() {
-            //     speed = speed++;
-            //   });
-
-            // }, icon: Icon(Icons.forward))
           ],
-        ),
+        )
       ],
     );
     // Use a FutureBuilder to display a loading spinner while waiting for the
